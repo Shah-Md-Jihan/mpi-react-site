b7dbbb4321fb063319e61a3f04e06ab284ffd896
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-10 10:10:57 +0600

    header component and some package installation done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is header</h1>
+            {/* <Link to="/academic">Academic</Link>
+            <Link to="/students">Student's Info</Link>
+            <Link to="/teacthers">Teacher's Info</Link>
+            <Link to="/notice">Notice</Link>
+            <Link to="/elearning">E-Learning</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/about">About</Link> */}
+        </div>
+    );
+};
+
+export default Header;
